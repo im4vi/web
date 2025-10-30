c506d36fe04411e84771904b48dcf1a28ea5dc82
im4vi <imjustavi16@gmail.com>
2025-10-30 12:43:39 +0100

    deploy: actualización automática jue 30 oct 2025 12:43:39 CET

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
+set -e
 
 # Generar el sitio
+echo "Generando sitio..."
 bundle exec jekyll build
 
 # Subir al repo de despliegue
+
+echo "Subiendo contenido estático a GitHub"
 cd ~/web
 git add .
 git commit -m "deploy: actualización automática $(date)"
 git push origin main
 
-echo "Sitio desplegado correctamente"
+cd ..
+echo "Sitio desplegado correctamente. Netlify actualizará automáticamente el sitio."
