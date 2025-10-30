1210ce52db551583818a6396580f05fadcebc7a3
im4vi <imjustavi16@gmail.com>
2025-10-30 12:32:27 +0100

    chore: cambio examen

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,12 @@
 # Generar el sitio
 bundle exec jekyll build
 
-# Limpiar carpeta de deploy
-rm -rf ~/web-deploy/*
-cp -r _site/* ~/web-deploy/
+# Limpiar carpeta de web
+rm -rf ~/web/*
+cp -r _site/* ~/web/
 
 # Subir al repo de despliegue
-cd ~/web-deploy
+cd ~/web
 git add .
 git commit -m "deploy: actualización automática $(date)"
 git push origin main
